4886b6516aecb656ee514b7a19687a096689aac1
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-04 20:49:04 +0100

    Fallback to downloading via file_get_contents() if curl is not availible

--- simpleCache.php
+++ simpleCache.php
@@ -58,14 +58,20 @@ class SimpleCache {
 	//Helper function for retrieving data from url
 	function do_curl($url)
 	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-		$content = curl_exec($ch);
-		curl_close($ch);
-		
+		if(function_exists("curl_init"))
+		{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+			$content = curl_exec($ch);
+			curl_close($ch);
+			
 		return $content;
+		}
+		else {
+			return file_get_contents($url);
+		}
 	}
 	
 	//Helper function to validate filenames

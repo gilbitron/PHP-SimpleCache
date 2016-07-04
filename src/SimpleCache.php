a3e9bbbcc882292508d08ffce3f7ab2a65a99b94
Alex Corbi <a.corbi@gmail.com>
2016-07-04 11:37:00 +0200

    Making CURL follow redirections

--- src/SimpleCache.php
+++ src/SimpleCache.php
@@ -63,6 +63,7 @@ class SimpleCache {
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 			$content = curl_exec($ch);
 			curl_close($ch);
